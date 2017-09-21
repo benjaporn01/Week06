7d45986a0e07a4ab255509e2f761876da384828d
benjaporn01 <59030179@kmitl.ac.th>
2017-09-21 21:26:13 +0700

    add  even & odd

--- Lap03/Lap03/Program.cs
+++ Lap03/Lap03/Program.cs
@@ -1,1 +1,25 @@
 ﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+        Console.Write("Enter number:");
+        num = int.Parse(Console.ReadLine());
+        check(ref num);
+        Console.WriteLine(num);
+        Console.ReadLine();
+    }
+    static void check(ref int i)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine("Even number");
+        }
+        else
+            Console.WriteLine("Odd number");
+    }
+
+}
